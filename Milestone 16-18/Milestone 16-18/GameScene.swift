b94626b3590fd94209765e7b39be85220ae33cd2
Kiny <89192734912nik@gmail.com>
2023-02-26 23:17:38 +0300

    init

---
+++ Milestone 16-18/Milestone 16-18/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Milestone 16-18
+//
+//  Created by nikita on 26.02.2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+        
+    override func didMove(to view: SKView) {
+      
+        }
+        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
